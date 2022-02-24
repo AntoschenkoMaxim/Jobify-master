fb0d8528cae627ed1aa1abaeb5ecc2a498de612b
AntoschenkoMaxim <jurusski@gmail.com>
2022-02-24 03:59:49 +0300

    added BarChart and AreaChart for statistic and filling the database with vacancies using populate.js and mockaroo

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BarChart from './BarChart'
+import AreaChart from './AreaChart'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/ChartsContainer'
 
-const ChartsContainer = () => {
-  return (
-	<div>
-		<h1>charts container</h1>
-	</div>
-  )
+export default function ChartsContainer() {
+	const [barChart, setBarChart] = useState(true) //устанавливаем состояние через хук useState
+	const { monthlyApplications: data } = useAppContext()
+
+	return (
+		<Wrapper>
+			<h4>Месячная статистика</h4>
+			{/* Выбираем тип диаграммы */}
+			<button type='button' onClick={() => setBarChart(!barChart)}>
+				{barChart ? 'Диаграмма области' : 'Столбчатая диаграмма'}
+			</button>
+			{/* Выбираем тип диаграммы */}
+			{barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+		</Wrapper>
+	)
 }
-
-export default ChartsContainer
